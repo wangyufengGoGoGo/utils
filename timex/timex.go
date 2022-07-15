0a373a7b797ccac2819ca5d391008cefe0fe64d1
wangyufengx <1101761521@qq.com>
2022-07-15 09:50:48 +0800

    添加简单时间函数

---
+++ timex/timex.go
@@ -0,0 +1,44 @@
+package timex
+
+import "time"
+
+const (
+	TimeFormat = "2006-01-02 15:04:05"
+)
+
+func SecondDuration(interval int64) time.Duration {
+	return time.Duration(interval) * time.Second
+}
+
+func MinuteDuration(interval int64) time.Duration {
+	return time.Duration(interval) * time.Minute
+}
+
+func HourDuration(interval int64) time.Duration {
+	return time.Duration(interval) * time.Hour
+}
+
+//After 当前时间在t之后
+func After(t time.Time) bool {
+	return time.Now().After(t)
+}
+
+//SecondAfter unix转换后时间加interval在当前时间之后
+func SecondAfter(unix, interval int64) bool {
+	return After(time.Unix(unix, 10).Add(SecondDuration(interval)))
+}
+
+//MinuteAfter unix转换后时间加interval在当前时间之后
+func MinuteAfter(unix, interval int64) bool {
+	return After(time.Unix(unix, 10).Add(MinuteDuration(interval)))
+}
+
+//HourAfter unix转换后时间加interval在当前时间之后
+func HourAfter(unix, interval int64) bool {
+	return After(time.Unix(unix, 10).Add(HourDuration(interval)))
+}
+
+//AfterByUnix unix转换后时间加t在当前时间之后
+func AfterByUnix(unix int64, t time.Duration) bool {
+	return After(time.Unix(unix, 10).Add(t))
+}
---
+++ timex/timex_test.go
@@ -0,0 +1,124 @@
+package timex
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestSecondDuration(t *testing.T) {
+	duration := SecondDuration(60)
+	second := 60 * time.Second
+	if duration != second {
+		t.Fatalf("duration error, %s not equal %s", duration, second)
+	}
+	fmt.Println(duration)
+}
+
+func TestMinuteDuration(t *testing.T) {
+	duration := MinuteDuration(60)
+	minute := 60 * time.Minute
+	if duration != minute {
+		t.Fatalf("duration error, %s not equal %s", duration, minute)
+	}
+	fmt.Println(duration)
+}
+
+func TestHourDuration(t *testing.T) {
+	duration := HourDuration(60)
+	hour := 60 * time.Hour
+	if duration != hour {
+		t.Fatalf("duration error, %s not equal %s", duration, hour)
+	}
+	fmt.Println(duration)
+}
+
+func TestAfter(t *testing.T) {
+	t.Run("当前时间之前", func(t *testing.T) {
+		start, err := time.Parse(TimeFormat, "2022-07-08 15:04:05")
+		if err != nil {
+			t.Fatalf("parse time error: {%s}", err)
+		}
+
+		if !After(start) {
+			t.Fatalf("start should less than now")
+		}
+	})
+	t.Run("当前时间之后", func(t *testing.T) {
+		if After(time.Now()) {
+			t.Fatalf("start should less than now")
+		}
+	})
+}
+
+func TestSecondAfter(t *testing.T) {
+	t.Run("当前时间之前", func(t *testing.T) {
+		start, err := time.Parse(TimeFormat, "2022-07-08 15:04:05")
+		if err != nil {
+			t.Fatalf("parse time error: {%s}", err)
+		}
+
+		if !SecondAfter(start.Unix(), 60) {
+			t.Fatalf("start should less than now")
+		}
+	})
+	t.Run("当前时间之后", func(t *testing.T) {
+		if SecondAfter(time.Now().Unix(), 60) {
+			t.Fatalf("start should less than now")
+		}
+	})
+}
+
+func TestMinuteAfter(t *testing.T) {
+	t.Run("当前时间之前", func(t *testing.T) {
+		start, err := time.Parse(TimeFormat, "2022-07-08 15:04:05")
+		if err != nil {
+			t.Fatalf("parse time error: {%s}", err)
+		}
+
+		if !MinuteAfter(start.Unix(), 60) {
+			t.Fatalf("start should less than now")
+		}
+	})
+	t.Run("当前时间之后", func(t *testing.T) {
+		if MinuteAfter(time.Now().Unix(), 60) {
+			t.Fatalf("start should less than now")
+		}
+	})
+}
+
+func TestHourAfter(t *testing.T) {
+	t.Run("当前时间之前", func(t *testing.T) {
+		start, err := time.Parse(TimeFormat, "2022-07-08 15:04:05")
+		if err != nil {
+			t.Fatalf("parse time error: {%s}", err)
+		}
+
+		if !HourAfter(start.Unix(), 60) {
+			t.Fatalf("start should less than now")
+		}
+	})
+	t.Run("当前时间之后", func(t *testing.T) {
+		if HourAfter(time.Now().Unix(), 60) {
+			t.Fatalf("start should less than now")
+		}
+	})
+}
+
+func TestAfterByUnix(t *testing.T) {
+	t.Run("当前时间之前", func(t *testing.T) {
+		start, err := time.Parse(TimeFormat, "2022-07-08 15:04:05")
+		if err != nil {
+			t.Fatalf("parse time error: {%s}", err)
+		}
+
+		if !AfterByUnix(start.Unix(), 60*time.Second) {
+			t.Fatalf("start should less than now")
+		}
+	})
+	t.Run("当前时间之后", func(t *testing.T) {
+		if AfterByUnix(time.Now().Unix(), 60*time.Second) {
+			t.Fatalf("start should less than now")
+		}
+	})
+}
