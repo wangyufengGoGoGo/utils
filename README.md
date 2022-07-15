63f8539d3e29b6717cef47de0222af203c8f13c0
wangyufengx <1101761521@qq.com>
2022-07-15 16:21:14 +0800

    添加base编码

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # utils
 golang utils
 
+- arrays
 - crypt
+  - base32\base64
   - md5
 - error
 - time
---
+++ crypt/base64.go
@@ -0,0 +1,22 @@
+package crypt
+
+import (
+	"encoding/base32"
+	"encoding/base64"
+)
+
+func Base32Encode(data []byte) string {
+	return base32.StdEncoding.EncodeToString(data)
+}
+
+func Base32Decode(data string) ([]byte, error) {
+	return base32.StdEncoding.DecodeString(data)
+}
+
+func Base64Encode(data []byte) string {
+	return base64.StdEncoding.EncodeToString(data)
+}
+
+func Base64Decode(data string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(data)
+}
